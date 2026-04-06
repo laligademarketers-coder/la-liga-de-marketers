35d1aada2d82a68ef398a9f81933cd447b875146
laligademarketers-coder <laligademarketers@gmail.com>
2026-04-06 00:36:56 +0000

    Add Meta Pixel to React index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Load Meta Pixel
+window.fbq = window.fbq || function() { 
+  window.fbq.queue = window.fbq.queue || [];
+  window.fbq.queue.push(arguments);
+};
+window.fbq('init', '1282058290533379');
+window.fbq('track', 'PageView');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
