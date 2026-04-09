7f77eef50eac17c93cf27913c2d8bbdb80d57d8a
laligademarketers-coder <laligademarketers@gmail.com>
2026-04-09 20:42:01 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ window.fbq = window.fbq || function() {
   window.fbq.queue = window.fbq.queue || [];
   window.fbq.queue.push(arguments);
 };
-window.fbq('init', '1282058290533379');
+window.fbq('init', '1265283559068729');
 window.fbq('track', 'PageView');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
